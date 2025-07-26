29905449fcb2a446074875edec297df35ccaf477
dexteer22 <lindamariaoffice@gmail.com>
2025-07-26 10:48:35 -0500

    Punto de teletransporte

--- codigo/minecart.gd
+++ codigo/minecart.gd
@@ -2,9 +2,12 @@ extends Sprite2D
 
 @onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+signal meta_activada
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	print("Meta alcanzada!!")
 	
 	collision_shape_2d.set_deferred("disabled", true)
-	InputManager.input_enabled = false
-	(body as CharacterBody2D)
+	meta_activada.emit()
+	
+	
---
+++ codigo/procesar_llegar_a_final_del_nivel.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready() -> void:
+	var minecart = get_tree().get_current_scene().get_node_or_null("minecart")
+	if minecart:
+		minecart.meta_activada.connect(_on_meta_activada)
+	else:
+		print("ERROR: No se ha encontrado en la escena el nodo 'Minecart', que debe ser hijo del nodo raíz de la misma.")
+	
+func _on_meta_activada() -> void:
+	InputManager.input_enabled = false
+	get_parent().velocity = Vector2.ZERO
+	
