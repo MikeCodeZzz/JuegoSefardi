730d7478074ba38f82254807149d5134cf61d61f
MoneyPredator <zopotamadre290@gmail.com>
2025-07-26 11:49:49 -0500

    Sonido de muerte

--- codigo/jugador.gd
+++ codigo/jugador.gd
@@ -4,6 +4,8 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var salto: AudioStreamPlayer2D = $Salto
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if InputManager.is_action_just_pressed("saltar") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		salto.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
--- codigo/zona_de_muerte.gd
+++ codigo/zona_de_muerte.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var sonido_muerte: AudioStreamPlayer2D = $SonidoMuerte
 
 func _on_body_entered(body: Node2D) -> void:
+	sonido_muerte.play()
 	print("Has perdido!")
 	timer.start()
 
