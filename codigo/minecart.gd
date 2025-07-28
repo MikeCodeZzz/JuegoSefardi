2ef0941c5b7532eca35d4a4c575962884ece1cd2
Santi <Santi@Oficina.local>
2025-07-28 08:59:03 -0500

    Musica win

--- codigo/minecart.gd
+++ codigo/minecart.gd
@@ -1,12 +1,12 @@
 extends Sprite2D
 
 @onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@export var musica_final: AudioStream
 
 signal meta_activada
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
-	print("Meta alcanzada!!")
-	
 	collision_shape_2d.set_deferred("disabled", true)
 	meta_activada.emit()
-	
+	if musica_final:
+		Musica.cambiar_musica(musica_final)
